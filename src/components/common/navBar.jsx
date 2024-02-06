c8dfded38cb485d71d65b48f35ebe3009aece67b
mhdi_nzari <mahdi.nazari.gb008@gmail.com>
2024-02-06 06:59:54 +0330

    init project

---
+++ src/components/common/navBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./styles/navBar.css";
+
+const NavBar = (props) => {
+  const { active } = props;
+
+  return (
+    <React.Fragment>
+      <div className="nav-container">
+        <nav className="navbar">
+          <div className="nav-background">
+            <ul className="nav-list">
+              <li
+                className={active === "home" ? "nav-item active" : "nav-item"}
+              >
+                <Link to="/">خانه</Link>
+              </li>
+              <li
+                className={active === "about" ? "nav-item active" : "nav-item"}
+              >
+                <Link to="/about">من کیم ؟</Link>
+              </li>
+              <li
+                className={
+                  active === "projects" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/projects">پروژه هام</Link>
+              </li>
+              <li
+                className={
+                  active === "articles" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/articles">تجربه هام</Link>
+              </li>
+              <li
+                className={
+                  active === "contact" ? "nav-item active" : "nav-item"
+                }
+              >
+                <Link to="/contact">ارتباط </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default NavBar;
---
+++ src/data/user.js
@@ -0,0 +1,92 @@
+const INFO = {
+  main: {
+    title: "وبسایت شخصی مهدی نظری<",
+    name: "مهدی نظری",
+    email: "nazari.eng@gmail.com",
+    logo: "../logo.png",
+  },
+
+  socials: {
+    twitter: "https://twitter.com/",
+    github: "https://github.com/",
+    linkedin: "https://linkedin.com/",
+    instagram: "https://instagram.com/",
+    stackoverflow: "https://stackoverflow.com/",
+    facebook: "https://facebook.com/",
+  },
+
+  homepage: {
+    title: "برنامه نویس فرانت اند  طراح سایت  پادکستر و تولید کننده محتوا",
+
+      
+    description:
+     "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای "
+  },
+
+  about: {
+    title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+    description:
+      "I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+  },
+
+  articles: {
+    title:
+      "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+    description:
+      "Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+  },
+
+  projects: [
+    {
+      title: "Project 1",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 2",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 3",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 4",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+
+    {
+      title: "Project 5",
+      description:
+        "Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+      logo:
+        "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+      linkText: "View Project",
+      link: "https://github.com",
+    },
+  ],
+};
+
+export default INFO;
